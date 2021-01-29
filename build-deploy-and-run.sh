1859b25b4cc42447cb3098b99d5932662918fd2c
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2021-01-29 18:03:53 +0100

    parameterized with cert and dev env

--- build-deploy-and-run.sh
+++ build-deploy-and-run.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
+while getopts "e:h" OPT; do
+    case $OPT in
+        e  )
+            env=$OPTARG
+            echo "env=$env"
+            if ! [[  -f "bin/$env/spark.properties" ]]
+            then
+                echo "Environment $env is not defined. Use -h for more details."
+                exit 1
+            fi
+            ;;
+        h  ) syntax
+             exit 0 ;;
+        ?  ) syntax
+             exit 1 ;;
+    esac
+done
+shift $(($OPTIND -1))
+
+if [ "$env" == "" ]
+then
+  echo "Missing -e [dev|cert]"
+  exit 1
+fi
+
 app=my-apache-spark-java-0.1-SNAPSHOT
-mvn clean package -Pmake-dist && \
-scp target/$app-bin.tar.gz dev1: && \
-ssh dev1 "rm -rf $app && tar xzf $app-bin.tar.gz && cd $app/bin && ./run.sh"
+mvn clean package -Pmake-dist -Denv=$env && \
+scp target/$app-bin.tar.gz "$env"1: && \
+ssh "$env"1 "rm -rf $app && tar xzf $app-bin.tar.gz && cd $app/bin && ./run.sh"
