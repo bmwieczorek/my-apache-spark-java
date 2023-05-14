06a9a86c23199d5123e0107f19f8a48e0aa06be9
Bartosz Wieczorek <bmwieczorek@gmail.com>
2023-05-14 17:41:26 +0200

    minor refactoring

--- src/main/java/com/bawi/spark/SparkApp.java
+++ src/main/java/com/bawi/spark/SparkApp.java
@@ -3,8 +3,26 @@ package com.bawi.spark;
 import com.bawi.spark.common.Configuration;
 import com.bawi.spark.common.ConfigurationProvider;
 import com.bawi.spark.common.SparkBase;
+//import com.codahale.metrics.Counter;
+//import com.codahale.metrics.MetricRegistry;
 
 public class SparkApp implements SparkBase, Read, Write, LoggingInfoListenerRegistrar, CustomSparkMetricsRegistrar, ConfigurationProvider {
+
+//    static class MySource implements org.apache.spark.metrics.source.Source {
+//
+//        @Override
+//        public String sourceName() {
+//            return "MySource";
+//        }
+//
+//        @Override
+//        public MetricRegistry metricRegistry() {
+//            return new MetricRegistry();
+//        }
+//
+//        public static final MetricRegistry METRIC_REGISTRY = new MetricRegistry();
+//        public static final Counter COUNTER = METRIC_REGISTRY.counter(MetricRegistry.name("fooCounter"));
+//    }
 
     public static void main(String[] args) {
         new SparkApp().start();
