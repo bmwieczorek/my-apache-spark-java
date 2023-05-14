82b0e580c55da142323be66fe559c5c8840d2983
Bartosz Wieczorek <bmwieczorek@gmail.com>
2023-05-14 17:46:57 +0200

    using groupon UserMetricsSystem

--- src/main/java/com/bawi/spark/MyCustomSparkMetricsRegistrar.java
+++ src/main/java/com/bawi/spark/MyCustomSparkMetricsRegistrar.java
@@ -3,20 +3,24 @@ package com.bawi.spark;
 import com.bawi.spark.common.CustomSparkMetricsListener;
 import com.bawi.spark.common.SparkBase;
 import org.apache.spark.SparkContext;
+import org.apache.spark.SparkEnv;
 import org.apache.spark.groupon.metrics.UserMetricsSystem;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public interface CustomSparkMetricsRegistrar extends SparkBase {
-    Logger LOGGER = LoggerFactory.getLogger(CustomSparkMetricsRegistrar.class);
+public interface MyCustomSparkMetricsRegistrar extends SparkBase {
+    Logger LOGGER = LoggerFactory.getLogger(MyCustomSparkMetricsRegistrar.class);
 
     @Override
     default void setupMetrics(SparkSession sparkSession) {
         SparkContext sparkContext = sparkSession.sparkContext();
-        UserMetricsSystem.initialize(sparkContext, "custom_metrics"); // works only with spark scala 2.11
+//        SparkApp.MySource mySource = new SparkApp.MySource();
+//        SparkEnv.get().metricsSystem().registerSource(mySource);
+
+        UserMetricsSystem.initialize(sparkContext, "custom_metrics");
         sparkContext.addSparkListener(new CustomSparkMetricsListener((key, value) -> {
-            LOGGER.info("Metric {} : {}", key, value);
+            LOGGER.debug("Metric {} : {}", key, value);
             UserMetricsSystem.counter(key).inc(value);
         }));
     }
