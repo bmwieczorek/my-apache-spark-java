cdd230c980b1995edac78afa9d56915cabe96b76
Bartosz Wieczorek <bmwieczorek@gmail.com>
2023-05-14 17:39:58 +0200

    minor refactoring

--- src/main/java/com/bawi/spark/common/SparkBase.java
+++ src/main/java/com/bawi/spark/common/SparkBase.java
@@ -1,26 +1,31 @@
 package com.bawi.spark.common;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.sql.SparkSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public interface SparkBase {
-    Logger LOGGER = LoggerFactory.getLogger(SparkBase.class);
 
     default void setupMetrics(SparkSession sparkSession) {}
     void runSpark(SparkSession sparkSession);
 
     default void start() {
-        SparkSession sparkSession = SparkSession.builder()
-                .appName(getClass().getSimpleName())
-                .master("local[*]")
-                .getOrCreate();
+        SparkConf sparkConf = new SparkConf().setAppName(getClass().getSimpleName());
+        if (isLocal()) {
+            sparkConf.setMaster("local[*]");
+        }
+
+        SparkSession sparkSession = SparkSession.builder().config(sparkConf).getOrCreate();
 
         setupMetrics(sparkSession);
 
         runSpark(sparkSession);
 
         sparkSession.stop();
+    }
+
+    static boolean isLocal() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        return osName.contains("mac") || osName.contains("windows");
     }
 
 }
